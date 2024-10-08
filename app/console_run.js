4f6f75df154f80bb81f67d9ced1a254781346c34
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-08 22:35:27 +0700

    Update console_run.js

--- app/console_run.js
+++ app/console_run.js
@@ -2,11 +2,78 @@ let tasks;
 let player;
 const getPlayerInfo = async () => {
   try {
-    const _0x16f2b4 = await fetch("https://www.vanadatahero.com/api/player", {
+    const _0x2e2dc3 = await fetch('https://www.vanadatahero.com/api/player', {
+      'cache': 'default',
+      'credentials': "include",
+      'headers': {
+        'Accept': "*/*",
+        'Accept-Language': "en-GB,en-US;q=0.9,en;q=0.8",
+        'Content-Type': 'application/json',
+        'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko)",
+        'X-Telegram-Web-App-Init-Data': JSON.parse(sessionStorage.getItem("__telegram__initParams")).tgWebAppData
+      },
+      'method': 'GET',
+      'mode': 'cors',
+      'redirect': "follow",
+      'referrer': "https://www.vanadatahero.com/home",
+      'referrerPolicy': "strict-origin-when-cross-origin"
+    });
+    if (_0x2e2dc3.ok) {
+      console.log("-> Successfully Get Player Info");
+      const _0x124d29 = await _0x2e2dc3.json();
+      player = _0x124d29;
+    } else {
+      if (_0x2e2dc3.status == 0x190) {
+        const _0x2df63b = await _0x2e2dc3.json();
+        console.log(_0x2df63b.message);
+      } else {
+        console.log("-> Error Get Player Info : " + _0x2e2dc3.status + '-' + _0x2e2dc3.statusText);
+      }
+    }
+  } catch (_0x4769a9) {
+    console.log(_0x4769a9);
+  }
+};
+const startMining = async () => {
+  try {
+    const _0x2247e8 = await fetch('https://www.vanadatahero.com/api/tasks/1', {
+      'body': "{\"status\":\"completed\",\"points\":100}",
       'cache': "default",
       'credentials': "include",
       'headers': {
-        'Accept': '*/*',
+        'Accept': "*/*",
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'Content-Type': 'application/json',
+        'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko)",
+        'X-Telegram-Web-App-Init-Data': JSON.parse(sessionStorage.getItem('__telegram__initParams')).tgWebAppData
+      },
+      'method': "POST",
+      'mode': 'cors',
+      'redirect': 'follow',
+      'referrer': "https://www.vanadatahero.com/home",
+      'referrerPolicy': "strict-origin-when-cross-origin"
+    });
+    if (_0x2247e8.ok) {
+      console.log("-> Successfully Start Mining");
+    } else {
+      if (_0x2247e8.status == 0x190) {
+        const _0x5359b7 = await _0x2247e8.json();
+        console.log(_0x5359b7.message);
+      } else {
+        console.log("-> Error Start Mining : " + _0x2247e8.status + '-' + _0x2247e8.statusText);
+      }
+    }
+  } catch (_0x53ec01) {
+    console.log(_0x53ec01);
+  }
+};
+const getTask = async () => {
+  try {
+    const _0x197fd9 = await fetch("https://www.vanadatahero.com/api/tasks", {
+      'cache': 'default',
+      'credentials': "include",
+      'headers': {
+        'Accept': "*/*",
         'Accept-Language': "en-GB,en-US;q=0.9,en;q=0.8",
         'Content-Type': 'application/json',
         'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko)",
@@ -14,138 +81,69 @@ const getPlayerInfo = async () => {
       },
       'method': "GET",
       'mode': "cors",
-      'redirect': "follow",
+      'redirect': 'follow',
       'referrer': "https://www.vanadatahero.com/home",
       'referrerPolicy': "strict-origin-when-cross-origin"
     });
-    if (_0x16f2b4.ok) {
-      console.log("-> Successfully Get Player Info");
-      const _0x234885 = await _0x16f2b4.json();
-      player = _0x234885;
-    } else {
-      if (_0x16f2b4.status == 0x190) {
-        const _0x3a7cc6 = await _0x16f2b4.json();
-        console.log(_0x3a7cc6.message);
-      } else {
-        console.log("-> Error Get Player Info : " + _0x16f2b4.status + '-' + _0x16f2b4.statusText);
-      }
-    }
-  } catch (_0x5c790a) {
-    console.log(_0x5c790a);
-  }
-};
-const startMining = async () => {
-  try {
-    const _0xe32cfd = await fetch('https://www.vanadatahero.com/api/tasks/1', {
-      'body': "{\"status\":\"completed\",\"points\":100}",
-      'cache': "default",
-      'credentials': "include",
-      'headers': {
-        'Accept': "*/*",
-        'Accept-Language': "en-GB,en-US;q=0.9,en;q=0.8",
-        'Content-Type': "application/json",
-        'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko)",
-        'X-Telegram-Web-App-Init-Data': JSON.parse(sessionStorage.getItem("__telegram__initParams")).tgWebAppData
-      },
-      'method': 'POST',
-      'mode': "cors",
-      'redirect': "follow",
-      'referrer': "https://www.vanadatahero.com/home",
-      'referrerPolicy': 'strict-origin-when-cross-origin'
-    });
-    if (_0xe32cfd.ok) {
-      console.log("-> Successfully Start Mining");
-    } else {
-      if (_0xe32cfd.status == 0x190) {
-        const _0x53c014 = await _0xe32cfd.json();
-        console.log(_0x53c014.message);
-      } else {
-        console.log("-> Error Start Mining : " + _0xe32cfd.status + '-' + _0xe32cfd.statusText);
-      }
-    }
-  } catch (_0x3702b6) {
-    console.log(_0x3702b6);
-  }
-};
-const getTask = async () => {
-  try {
-    const _0x4c9a2b = await fetch("https://www.vanadatahero.com/api/tasks", {
-      'cache': "default",
-      'credentials': "include",
-      'headers': {
-        'Accept': "*/*",
-        'Accept-Language': "en-GB,en-US;q=0.9,en;q=0.8",
-        'Content-Type': "application/json",
-        'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko)",
-        'X-Telegram-Web-App-Init-Data': JSON.parse(sessionStorage.getItem("__telegram__initParams")).tgWebAppData
-      },
-      'method': 'GET',
-      'mode': "cors",
-      'redirect': "follow",
-      'referrer': "https://www.vanadatahero.com/home",
-      'referrerPolicy': "strict-origin-when-cross-origin"
-    });
-    if (_0x4c9a2b.ok) {
-      const _0x3679d6 = await _0x4c9a2b.json();
+    if (_0x197fd9.ok) {
+      const _0x400895 = await _0x197fd9.json();
       console.log("-> Successfully Get Task");
-      tasks = _0x3679d6.tasks.filter(_0x5e8db6 => _0x5e8db6.id != 0x1 && _0x5e8db6.id != 0x11 && _0x5e8db6.id != 0x5);
+      tasks = _0x400895.tasks.filter(_0x113d6a => _0x113d6a.id != 0x1 && _0x113d6a.id != 0x11 && _0x113d6a.id != 0x5);
     } else {
-      if (_0x4c9a2b.status == 0x190) {
-        const _0x5bff2c = await _0x4c9a2b.json();
-        console.log(_0x5bff2c.message);
+      if (_0x197fd9.status == 0x190) {
+        const _0x3e2661 = await _0x197fd9.json();
+        console.log(_0x3e2661.message);
       } else {
-        console.log("-> Error Get Tasks : " + _0x4c9a2b.status + '-' + _0x4c9a2b.statusText);
+        console.log("-> Error Get Tasks : " + _0x197fd9.status + '-' + _0x197fd9.statusText);
       }
     }
-  } catch (_0x216c41) {
-    console.log(_0x216c41);
+  } catch (_0x1f7c68) {
+    console.log(_0x1f7c68);
   }
 };
-const completeAndClaimTask = async _0x1f0e32 => {
+const completeAndClaimTask = async _0x1c51e4 => {
   try {
-    console.log("-> Try to Completing Task " + _0x1f0e32.name);
-    const _0x4712f8 = {
+    console.log("-> Try to Completing Task " + _0x1c51e4.name);
+    const _0xbb162f = {
       'status': "completed",
-      'points': _0x1f0e32.points
+      'points': _0x1c51e4.points
     };
-    const _0x230621 = await fetch("https://www.vanadatahero.com/api/tasks", {
-      'body': JSON.stringify(_0x4712f8),
-      'cache': 'default',
-      'credentials': 'include',
+    const _0x395448 = await fetch('https://www.vanadatahero.com/api/tasks', {
+      'body': JSON.stringify(_0xbb162f),
+      'cache': "default",
+      'credentials': "include",
       'headers': {
         'Accept': "*/*",
         'Accept-Language': "en-GB,en-US;q=0.9,en;q=0.8",
-        'Content-Type': "application/json",
+        'Content-Type': 'application/json',
         'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko)",
-        'X-Telegram-Web-App-Init-Data': JSON.parse(sessionStorage.getItem('__telegram__initParams')).tgWebAppData
+        'X-Telegram-Web-App-Init-Data': JSON.parse(sessionStorage.getItem("__telegram__initParams")).tgWebAppData
       },
       'method': "POST",
       'mode': "cors",
       'redirect': "follow",
       'referrer': "https://www.vanadatahero.com/home",
-      'referrerPolicy': 'strict-origin-when-cross-origin'
+      'referrerPolicy': "strict-origin-when-cross-origin"
     });
-    if (_0x230621.ok) {
-      console.log("-> Task " + _0x1f0e32.name + " Completed");
+    if (_0x395448.ok) {
+      console.log("-> Task " + _0x1c51e4.name + " Completed");
     } else {
-      if (_0x230621.status == 0x190) {
-        const _0xdf5a11 = await _0x230621.json();
-        console.log(_0xdf5a11.message);
+      if (_0x395448.status == 0x190) {
+        const _0x3185ab = await _0x395448.json();
+        console.log(_0x3185ab.message);
       } else {
-        console.log("-> Failed to Complete task " + _0x1f0e32.name + " : " + _0x230621.status + '-' + _0x230621.statusText);
+        console.log("-> Failed to Complete task " + _0x1c51e4.name + " : " + _0x395448.status + '-' + _0x395448.statusText);
       }
     }
-  } catch (_0x5d1ae8) {
-    console.log(_0x5d1ae8);
+  } catch (_0x4bccbf) {
+    console.log(_0x4bccbf);
   }
 };
 const runScript = async () => {
-  console.log('==============================');
-  console.log("=     VANA DATA HERO Bot     =");
-  console.log("=     Created by Widiskel    =");
   console.log("==============================");
-  console.log();
-  console.log("                                 \n                      ..                                 \n                     .;:.                                 \n                    .;ol,.                                \n                   .;ooc:'                                \n            ..    .;ooccc:'.    ..                        \n          .',....'cdxlccccc;.....,'.                      \n         .;;..'';clolccccccc:,''..;;.                     \n        ':c'..':cccccccccccccc;...'c:.                    \n       ':cc,.'ccccccccccccccccc:..;cc:'                   \n    ...:cc;.':cccccccccccccccccc:..:cc:...                \n   .;';cc;.':;;:cccccccccccccc:;;;'.;cc,,;.               \n  .cc':c:.',.....;cccccccccc;.....,..:c:'c:               \n  ,x:'cc;.,'     .':cccccc:'.     ',.;cc':x'              \n  lO,'cc;.;,       .;cccc:.       ,;.;cc';0l              \n .o0;.;c;.,:'......',''''''......':,.;c;.:0l.             \n .lxl,.;,..;c::::;:,.    .,:;::::c;..,;.,oxl.             \n .lkxOl..  ..'..;::'..''..'::;..'..  ..c0xkl.             \n  .cKMx.        .;c:;:cc:;:c:.        .xMKc.              \n    ;KX:         ;o::l:;cc;o:.        ;KK;                \n     :KK:.       ,d,cd,'ol'o:       .:0K:                 \n      ;0NOl:;:loo;. ... .. .;ldlc::lkN0:                  \n       .lONNNKOx0Xd,;;'.,:,lKKkk0XNN0o.                   \n         .','.. .lX0doooodOXd.  .','.                     \n                 .,okkddxkd;.                             \n                    'oxxd;.                               \n   ........................................                              \n   .OWo  xNd lox  xxl Ald   xoc dakkkkkxsx.              \n   .OWo  o0W cXW  dM0 MMN   lNK laddKMNkso.               \n   .kMKoxsNN oWX  dW0 MMMWO lWK    axM0   .                \n   .OMWXNaMX dM0  kM0 MMKxNXKW0    axMk   .                 \n   .OMk  dWK oWX XWdx Mxx  XMMO    akMx   .                 \n   'OWo  dM0 'kNNXNNd DMD   OWk    aoWd   .                 \n   ........................................                 \n                                                                      \n");
+  console.log("=     VANA DATA HERO Bot     =");
+  console.log("=         VIKITOSHI          =");
+  console.log("==============================");
   console.log("-> Getting Player Information");
   await getPlayerInfo();
   if (player) {
@@ -157,22 +155,22 @@ const runScript = async () => {
   console.log("-> Getting Available Task");
   await getTask();
   if (tasks.length > 0x0) {
-    const _0x1f2d38 = tasks.filter(_0x57448c => _0x57448c.completed.length == 0x0);
-    for (const _0x56cae7 of _0x1f2d38) {
-      await completeAndClaimTask(_0x56cae7);
+    const _0x44fd2c = tasks.filter(_0x1e8f7c => _0x1e8f7c.completed.length == 0x0);
+    for (const _0x3bef33 of _0x44fd2c) {
+      await completeAndClaimTask(_0x3bef33);
     }
-    if (_0x1f2d38.length == 0x0) {
+    if (_0x44fd2c.length == 0x0) {
       console.log("-> All Task Completed");
     }
   }
-  let _0x42e609 = 0x0;
-  const _0x45aef0 = _0x57a93c => new Promise(_0x717409 => setTimeout(_0x717409, _0x57a93c));
+  let _0x1c1f25 = 0x0;
+  const _0x3b8441 = _0x7091b1 => new Promise(_0xe974af => setTimeout(_0xe974af, _0x7091b1));
   startMining();
-  while (_0x42e609 < 1800000) {
+  while (_0x1c1f25 < 1800000) {
     console.log("Delaying for 20 Seconds Before Tapping...");
-    await _0x45aef0(0x4e20);
+    await _0x3b8441(0x4e20);
     startMining();
-    _0x42e609 += 0x4e20;
+    _0x1c1f25 += 0x4e20;
   }
   console.log("Successfully Tapping for 30 Minutes");
   console.log("ACCOUNT Process complete");
