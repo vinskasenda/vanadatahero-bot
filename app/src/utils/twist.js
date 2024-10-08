086634caa45adc0fa93caedf8bb220c666e0a743
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-08 22:40:51 +0700

    Update twist.js

--- app/src/utils/twist.js
+++ app/src/utils/twist.js
@@ -1,36 +1,36 @@
 import { Twisters } from 'twisters';
-import a7_0x2f7216 from './logger.js';
+import a7_0x3a9b7d from './logger.js';
 import { Core } from '../core/core.js';
 class Twist {
   constructor() {
     this.twisters = new Twisters();
   }
-  ["log"](_0x4c30eb = '', _0x12d6e5 = '', _0x3bb48b = new Core(), _0x1457d9) {
-    if (_0x1457d9 == undefined) {
-      a7_0x2f7216.info(_0x12d6e5.id + " - " + _0x4c30eb);
-      _0x1457d9 = '-';
+  ["log"](_0x5766a6 = '', _0x58b319 = '', _0x3f1900 = new Core(), _0xb8f4d5) {
+    if (_0xb8f4d5 == undefined) {
+      a7_0x3a9b7d.info(_0x58b319.id + " - " + _0x5766a6);
+      _0xb8f4d5 = '-';
     }
-    const _0x457c4a = _0x3bb48b.user ?? {};
-    const _0x33e441 = _0x457c4a.points ?? '-';
-    const _0x40e232 = _0x3bb48b.task ?? [];
-    const _0x141219 = _0x40e232.filter(_0x9313cf => _0x9313cf.completed.length > 0x0) ?? [];
-    const _0x3237ba = _0x141219.length ?? 0x0;
-    const _0x10534e = _0x40e232.filter(_0x77e457 => _0x77e457.completed.length == 0x0) ?? [];
-    const _0x4e5d97 = _0x10534e.length ?? 0x0;
-    this.twisters.put(_0x12d6e5.id, {
-      'text': "\n================= Account " + _0x12d6e5.id + " =============\nName         : " + (_0x12d6e5.firstName ?? "Unamed") + " " + (_0x12d6e5.lastName ?? '') + " \nPoint        : " + _0x33e441 + "\nTask         : Completed : " + _0x3237ba + " | Uncompleted : " + _0x4e5d97 + "\n\nStatus : " + _0x4c30eb + "\nDelay : " + _0x1457d9 + "\n=============================================="
+    const _0x28a034 = _0x3f1900.user ?? {};
+    const _0x3cd275 = _0x28a034.points ?? '-';
+    const _0x1a6892 = _0x3f1900.task ?? [];
+    const _0x598c63 = _0x1a6892.filter(_0x13bc43 => _0x13bc43.completed.length > 0x0) ?? [];
+    const _0x56215f = _0x598c63.length ?? 0x0;
+    const _0x5cce1c = _0x1a6892.filter(_0x68463e => _0x68463e.completed.length == 0x0) ?? [];
+    const _0x3ae48f = _0x5cce1c.length ?? 0x0;
+    this.twisters.put(_0x58b319.id, {
+      'text': "\n================= Account " + _0x58b319.id + " =============\nName         : " + (_0x58b319.firstName ?? "Unamed") + " " + (_0x58b319.lastName ?? '') + " \nPoint        : " + _0x3cd275 + "\nTask         : Completed : " + _0x56215f + " | Uncompleted : " + _0x3ae48f + "\n\nStatus : " + _0x5766a6 + "\nDelay : " + _0xb8f4d5 + "\n=============================================="
     });
   }
-  ["info"](_0x48df45 = '') {
+  ["info"](_0x3a9d00 = '') {
     this.twisters.put(0x2, {
-      'text': "\n==============================================\nInfo : " + _0x48df45 + "\n=============================================="
+      'text': "\n==============================================\nInfo : " + _0x3a9d00 + "\n=============================================="
     });
     return;
   }
   ["clearInfo"]() {
     this.twisters.remove(0x2);
   }
-  async ["clear"](_0x5e9517) {
+  async ["clear"](_0x3e311f) {
     await this.twisters.flush();
   }
 }
